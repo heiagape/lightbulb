fbacfe3bb403e557b04181fea18bc1786583a725
justinckh <justinckhcheung@hotmail.com>
2026-01-20 10:15:11 +0800

    physical

--- src/Branch.jsx
+++ src/Branch.jsx
@@ -715,26 +715,26 @@ function Branch() {
                   //   emissive="#FAF9D0"
                   //   emissiveIntensity={0.2}
                   // />
-                  <meshStandardMaterial
-                  color={"#ffffff"}
-                  opacity={0.05}
-                  transparent={true}
-                  />
-                  // <meshPhysicalMaterial
-                  //   color={"#ffffff"}
-                  //   // Transmission glass: keep opacity at 1 and drive the look via transmission/attenuation
-                  //   transparent={false}
-                  //   opacity={1.0}
-                  //   transmission={1.0}
-                  //   ior={1.5}
-                  //   // Make depth visible: larger thickness + attenuation tint
-                  //   thickness={0.1}
-                  //   attenuationDistance={0.2}
-                  //   attenuationColor={"#e4f3ff"}
-                  //   dispersion={1.0}
-                  //   roughness={0.02}
-                  //   side={THREE.DoubleSide}
+                  // <meshStandardMaterial
+                  // color={"#ffffff"}
+                  // opacity={0.05}
+                  // transparent={true}
                   // />
+                  <meshPhysicalMaterial
+                    color={"#ffffff"}
+                    // Transmission glass: keep opacity at 1 and drive the look via transmission/attenuation
+                    transparent={false}
+                    opacity={1.0}
+                    transmission={1.0}
+                    ior={1.5}
+                    // Make depth visible: larger thickness + attenuation tint
+                    thickness={0.1}
+                    attenuationDistance={0.2}
+                    attenuationColor={"#e4f3ff"}
+                    dispersion={1.0}
+                    roughness={0.02}
+                    side={THREE.DoubleSide}
+                  />
                 )}
                 {isTransparentMesh && (
                   // <ThinMiracleGlass
